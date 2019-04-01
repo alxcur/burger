5092fec882ac44c184fb76209cce9b999a6c9143
Alex Curington <alex.curington@me.com>
2019-03-31 20:14:06 -0500

    no message

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,3 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Patty Melt", false);
 INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", false);
-INSERT INTO burgers (burger_name, devoured) VALUES ("Triple Mack Attack", true);
